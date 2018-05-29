3a99a212d6e8b14248c904096696c21978f7f8ec
Thog <me@thog.eu>
2018-05-29 23:08:08 +0200

    Because we are the pid 8, we need to register ourself to fsp-lr in older versions (Fix #2)

--- source/main.c
+++ source/main.c
@@ -1,13 +1,46 @@
 #include <switch.h>
 
-// no doc about svcBreak, I guess it's the same as the 3ds?
-#define BREAK_PANIC 0x0
+#define ERPT_SAVE_ID 0x80000000000000D1
 
 // we aren't an applet
 u32 __nx_applet_type = AppletType_None;
 
 // setup a fake heap (we don't need the heap anyway)
-char   fake_heap[0x100];
+char   fake_heap[0x1000];
+
+void fatalLater(Result err)
+{
+#ifdef DEBUG
+    Handle srv;
+
+    while (R_FAILED(smGetServiceOriginal(&srv, smEncodeName("fatal:u"))))
+    {
+        // wait one sec and retry
+        svcSleepThread(1000000000L);
+    }
+
+    // fatal is here time, fatal like a boss    
+    IpcCommand c;
+    ipcInitialize(&c);
+    ipcSendPid(&c);
+    struct {
+        u64 magic;
+        u64 cmd_id;
+        u64 result;
+        u64 unknown;
+    } *raw;
+
+    raw = ipcPrepareHeader(&c, sizeof(*raw));
+
+    raw->magic = SFCI_MAGIC;
+    raw->cmd_id = 1;
+    raw->result = err;
+    raw->unknown = 0;
+
+    ipcDispatch(srv);
+    svcCloseHandle(srv);
+#endif
+}
 
 // we override libnx internals to do a minimal init
 void __libnx_initheap(void) {
@@ -16,7 +49,7 @@ void __libnx_initheap(void) {
 
     // setup newlib fake heap
     fake_heap_start = fake_heap;
-    fake_heap_end   = fake_heap + 100;
+    fake_heap_end   = fake_heap + 0x1000;
 }
 
 void __appInit(void) {
@@ -24,10 +57,10 @@ void __appInit(void) {
 
     rc = smInitialize();
     if (R_FAILED(rc))
-        svcBreak(BREAK_PANIC, rc, 0);
+        fatalLater(rc);
     rc = fsInitialize();
     if (R_FAILED(rc))
-        svcBreak(BREAK_PANIC, rc, 0);
+        fatalLater(rc);
 }
 
 void __appExit(void) {
@@ -66,10 +99,31 @@ Result fsDeleteSaveData(u64 saveID) {
     return rc;
 }
 
-int main(int argc, char **argv) {
-    // Delete erpt saves
-    Result rc = fsDeleteSaveData(0x80000000000000D1);
+void registerFspLr()
+{
+    if (kernelAbove400())
+        return;
+
+    Result rc = fsprInitialize();
     if (R_FAILED(rc))
-        svcBreak(BREAK_PANIC, rc, 0);
+        fatalLater(rc);
+
+    u64 pid;
+    svcGetProcessId(&pid, 0xFFFF8001);
+
+    rc = fsprRegisterProgram(pid, 0x4200000000000000, FsStorageId_NandSystem, NULL, 0, NULL, 0);
+    if (R_FAILED(rc))
+        fatalLater(rc);
+    fsprExit();
+}
+
+int main(int argc, char **argv) {
+    registerFspLr();
+    // Delete erpt saves
+    Result rc = fsDeleteSaveData(ERPT_SAVE_ID);
+
+    if (R_FAILED(rc))
+        fatalLater(rc);
+
     return 0;
 }
